c56c44b743bed83b17e21ca622ffe280e0d43c00
tgabriel22 <thierrygabriel87@gmail.com>
2024-08-28 07:49:12 -0400

    Upload final version Full_Stack_Project

---
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Navbar.css";
+// import navlogo from "../Assets/nav-logo.svg";
+import logo from "../Assets/CompanyLogoedit.jpg";
+import navprofileIcon from "../Assets/nav-profile.svg";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="nav-leftSide">
+        <img src={logo} alt="logo" />
+        <p className="logoText">
+          Lion'<span className="lightgreen">A</span>
+          <span className="pink">r</span>
+          <span className="yellow">t</span>
+          <br />
+          collection
+          <br />
+          <span className="admPanel">Admin Panel</span>
+        </p>
+      </div>
+      {/* <img src={navlogo} className="nav-logo" alt="" /> */}
+      <img src={navprofileIcon} className="nav-profile" alt="" />
+    </div>
+  );
+};
+
+export default Navbar;
