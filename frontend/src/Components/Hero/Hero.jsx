d22e56127da4ca1a58e7523d3e047072dffe6886
tgabriel22 <thierrygabriel87@gmail.com>
2024-08-29 00:40:48 -0400

    Update Hero-btn and frontend title

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -2,26 +2,31 @@ import React from "react";
 import "./Hero.css";
 // import hero_image from "../Assets/hero_image.png";
 import hero_image from "../Assets/womenTwo-pieceset Product.png";
-import hand_icon from "../Assets/hand_icon.png";
+// import hand_icon from "../Assets/hand_icon.png";
 import arrow_icon from "../Assets/arrow.png";
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-left">
-        <h2>NEW ARRIVALS ONLY</h2>
-        <div>
+        {/* <h2>NEW ARRIVALS ONLY</h2> */}
+        {/* <h2>Step into Comfort and Creativity</h2> */}
+        <div className="hero-title">
           <div className="hero-hand-icon">
-            <p>new</p>
-            <img src={hand_icon} alt="" />
+            <p>Discover </p>
+            {/* <img src={hand_icon} alt="" /> */}
           </div>
-          <p>collections</p>
-          <p>for everyone</p>
+          <p>the Latest</p>
+          <p>
+            in Crochet <br /> Fashion
+          </p>
         </div>
-        <div className="hero-latest-btn">
-          <div>Latest Collection</div>
-          <img src={arrow_icon} alt="" />
-        </div>
+        <a href="#new-collections">
+          <div className="hero-latest-btn">
+            <div>Latest Collection</div>
+            <img src={arrow_icon} alt="" />
+          </div>
+        </a>
       </div>
       <div className="hero-right">
         <img src={hero_image} alt="hero" />
--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import './NewCollections.css'
-import Item from '../Item/Item'
+import React from "react";
+import "./NewCollections.css";
+import Item from "../Item/Item";
 
 const NewCollections = (props) => {
   return (
-    <div className='new-collections'>
+    <div id="new-collections" className="new-collections">
       <h1>NEW COLLECTIONS</h1>
       <hr />
       <div className="collections">
-        {props.data.map((item,index)=>{
-                return <Item id={item.id} key={index} name={item.name} image={item.image}  new_price={item.new_price} old_price={item.old_price}/>
-            })}
+        {props.data.map((item, index) => {
+          return (
+            <Item
+              id={item.id}
+              key={index}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          );
+        })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NewCollections
+export default NewCollections;
